cafe8e0390052049b7bcb1b66e09a905009766b9
Kyle Chen <kchen727@uwo.ca>
2023-11-19 20:27:11 -0500

    restarting auth for game

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<main className="p-4">
+			<div>
+				<h1> Who's that Pokemon? </h1>
+			</div>
+		</main>
+	);
+}
--- src/app/auth.tsx
+++
@@ -1,2 +0,0 @@
-import React from "react";
-import firebase from "../../firebase/clientApp";
