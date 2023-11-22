fe42fb0849556b0381bd783a89c938718d262e29
Kyle Chen <kchen727@uwo.ca>
2023-11-21 22:54:30 -0500

    setup state for api calls on front end

--- app/api/pokemon.js
+++ app/api/pokemon.js
@@ -2,12 +2,13 @@ import axios from "axios";
 
 export default async function getPokemon(id) {
 	try {
-		// const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
-		const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/1`);
-		const data = await response.data.forms[0].name;
+		const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+		const name = await response.data.species.name;
+		const sprite = await response.data.sprites.front_default;
 		// console.log(data.forms[0].name); // bulbasaur
-		console.log(data);
-		return data;
+		const res = [name, sprite];
+		console.log(res);
+		return res;
 	} catch (error) {
 		console.log(error);
 	}
--- app/components/PokemonCard.jsx
+++ app/components/PokemonCard.jsx
@@ -5,12 +5,26 @@ import Image from "next/image";
 import getPokemon from "../api/pokemon";
 
 const PokemonCard = () => {
+	const [pokemonImg, setPokemonImg] = useState("Hi"); // Default Image will be a pokeball
+	const [pokemonName, setPokemonName] = useState(null);
+
+	const handleGetPokemon = async () => {
+		try {
+			const pokemon = await getPokemon(1);
+			setPokemonImg(pokemon[1]);
+			setPokemonName(pokemon[0]);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 	return (
 		<div>
-			{/* <Image src={pokemon} width="100" height="100" /> */}
+			{/* <Image src={pokemonImg} width="100" height="100" /> */}
 			PokemonCard
 			<div>
-				<button onClick={() => getPokemon(1)}>Get Pokemon</button>
+				<button onClick={handleGetPokemon}>Get Pokemon</button>
+				<p> {pokemonImg} </p>
+				<p> {pokemonName} </p>
 			</div>
 		</div>
 	);
